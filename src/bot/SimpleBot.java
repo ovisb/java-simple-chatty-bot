d86414fe45e783e42703d4dd91231f336f9b95a2
Ovidiu Sbangu <ovidiu.sbangu@gmail.com>
2023-10-01 13:58:27 +0300

    Complete project stage3

--- src/bot/SimpleBot.java
+++ src/bot/SimpleBot.java
@@ -10,9 +10,23 @@ public class SimpleBot {
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        // reading a name
-        String yourName = scanner.next();
+        String name = scanner.nextLine();
 
-        System.out.printf("What a great name you have, %s!\n", yourName);
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int yourAge = 0;
+
+        // reading all remainders
+        int rem1 = scanner.nextInt();
+        int rem2 = scanner.nextInt();
+        int rem3 = scanner.nextInt();
+
+        while (!(yourAge % 3 == rem1 && yourAge % 5 == rem2 && yourAge % 7 == rem3)) {
+            yourAge += 1;
+        }
+
+        System.out.printf("Your age is %d; that's a good time to start programming!", yourAge);
     }
 }
