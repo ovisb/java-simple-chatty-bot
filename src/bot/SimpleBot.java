a1ae03c4f3c476c7627c1091e013dce2764b98ec
Ovidiu Sbangu <ovidiu.sbangu@gmail.com>
2023-10-01 13:34:58 +0300

    Complete project stage1

---
+++ src/bot/SimpleBot.java
@@ -0,0 +1,11 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("""
+                Hello! My name is BotMAN.
+                I was created in 2023.
+                """);
+    }
+}
